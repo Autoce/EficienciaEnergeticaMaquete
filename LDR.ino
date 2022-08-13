188bcfbe73d7b4990f8d34ae350603e586165a16
Leomar Marcelo Marschalk <leomarmm1@gmail.com>
2022-08-12 22:38:50 -0300

    Devido a mudanças no hardware do protótipo, as atribuições de pinos foram alteradas.

--- LDR.ino
+++ LDR.ino
@@ -1,9 +1,11 @@
 #include <PID_v1.h>
 
-#define LDR0_PIN 34
-#define LDR1_PIN 35
-#define LDR2_PIN 32
-#define LDR3_PIN 33
+#define LDR0_PIN 33
+#define LDR1_PIN 32
+#define LDR2_PIN 35
+#define LDR3_PIN 34
+
+
 #define LED0_PIN 19
 #define LED1_PIN 21
 #define LED2_PIN 22
